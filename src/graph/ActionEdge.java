2c76f20527c6cf8aed9b2f1254679cd5b877454e
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2024-12-06 15:09:06 +0700

    fix naming, fix code

--- src/graph/ActionEdge.java
+++ src/graph/ActionEdge.java
@@ -8,33 +8,33 @@ public abstract class ActionEdge extends Pair<Node, Node> {
         super(x, y);
     }
     public abstract double getWeight();
-    public abstract String edgeType();
+    public abstract String getType();
 }
 class FollowEdge extends ActionEdge {
     public FollowEdge(Node x, Node y) {
         super(x, y);
     }
     public double getWeight() { return Constants.FOLLOW_EDGE_WEIGHT; }
-    public String edgeType() { return "FollowEdge"; }
+    public String getType() { return "FollowEdge"; }
 }
 class CommentEdge extends ActionEdge {
     public CommentEdge(Node x, Node y) {
         super(x, y);
     }
     public double getWeight() { return Constants.COMMENT_EDGE_WEIGHT; }
-    public String edgeType() { return "CommentEdge"; }
+    public String getType() { return "CommentEdge"; }
 }
 class TweetEdge extends ActionEdge {
     public TweetEdge(Node x, Node y) {
         super(x, y);
     }
     public double getWeight() { return Constants.TWEET_EDGE_WEIGHT; }
-    public String edgeType() { return "TweetEdge"; }
+    public String getType() { return "TweetEdge"; }
 }
 class RepostEdge extends ActionEdge {
     public RepostEdge(Node x, Node y) {
         super(x, y);
     }
     public double getWeight() { return Constants.REPOST_EDGE_WEIGHT; }
-    public String edgeType() { return "RepostEdge"; }
+    public String getType() { return "RepostEdge"; }
 }
